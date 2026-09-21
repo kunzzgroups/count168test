7a22c791e9dbcdd5c6c92be5a520cb8f947e3c2d
kunzzit02 <Kunzzit02@gmail.com>
2026-09-21 10:24:10 +0800

    0.4

---
+++ frontend/dist/assets/usePartnershipAuditWriteGuard-RfJ3a5Pe.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-Dy7U09iA.js";import{u as n,g as d}from"./partnershipAuditReadOnly-DbcQhx7d.js";function p(t,r,a){const u=n(t),o="Read-only account: this action is not allowed.",e=i.useCallback(()=>d(t,()=>{typeof r=="function"&&r(a||o,"danger")}),[t,r,a]);return{mutationsBlocked:u,guardWrite:e,isLocked:u}}export{p as u};
